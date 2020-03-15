1d86624c77468963c72f94ef763f2aa4e0ad79bc
Mark <markeddd19@gmail.com>
2020-03-15 21:26:02 +0100

    updated database context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,11 +9,5 @@ namespace Api.Data
 
         public DbSet<Movie> Movie { get; set; }
         public DbSet<Director> Director { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Director>().HasMany(d => d.Movies).WithOne(d => d.Director);
-            modelBuilder.Entity<Movie>().HasOne(m => m.Director).WithMany(m => m.Movies);
-        }
     }
 }
